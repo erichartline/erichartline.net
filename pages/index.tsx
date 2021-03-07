c6b7f9e798799c4191631f1f6342c347b4d7a76b
wildlifehexagon <hartline.eric@gmail.com>
2021-03-07 14:47:07 -0600

    refactor: convert to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head"
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Eric Hartline - Web Developer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>main content here...</main>
+
+      <footer>footer!</footer>
+    </div>
+  )
+}
+
+export default Home
