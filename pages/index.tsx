7a47a797c93c7fe6711370abae1d8c939416a11f
wildlifehexagon <hartline.eric@gmail.com>
2021-03-07 15:05:22 -0600

    refactor: autocenter containers, remove old files

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,9 @@ import Head from "next/head"
 
 const Home = () => {
   return (
-    <div>
+    <div className="container">
       <Head>
         <title>Eric Hartline - Web Developer</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>main content here...</main>
