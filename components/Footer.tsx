916f75e483a98ab781133f898499fff91f56319d
wildlifehexagon <hartline.eric@gmail.com>
2021-03-08 21:13:00 -0600

    refactor: remove unnecessary paragraph

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link"
-
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font my-4 text-center">
-      <p className="my-4">© {new Date().getFullYear()} Eric Hartline</p>
+      © {new Date().getFullYear()} Eric Hartline
     </footer>
   )
 }
