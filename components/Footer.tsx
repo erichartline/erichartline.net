f428abb9a7ce5d9df73330b6ff2806f6ed251089
wildlifehexagon <hartline.eric@gmail.com>
2021-03-30 22:18:51 -0500

    feat: add social links in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link"
+
+const links = [
+  {
+    name: "GitHub",
+    url: "https://github.com/wildlifehexagon",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/wildlifehexagon",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/erichartline/",
+  },
+]
+
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font my-4 text-center">
       © {new Date().getFullYear()} Eric Hartline
+      <div className="mt-2">
+        {links.map((item) => (
+          <Link key={item.name} href={item.url}>
+            <a className="mr-2">{item.name}</a>
+          </Link>
+        ))}
+      </div>
     </footer>
   )
 }
