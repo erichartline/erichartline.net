976102607f6875af2133382eddb8128b6243cf81
wildlifehexagon <hartline.eric@gmail.com>
2021-04-10 15:09:18 -0500

    feat: add social icon svgs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,32 +1,11 @@
-import Link from "next/link"
+import SocialIcons from "./SocialIcons"
 import NowPlaying from "./NowPlaying"
-
-const links = [
-  {
-    name: "GitHub",
-    url: "https://github.com/wildlifehexagon",
-  },
-  {
-    name: "Twitter",
-    url: "https://twitter.com/wildlifehexagon",
-  },
-  {
-    name: "LinkedIn",
-    url: "https://www.linkedin.com/in/erichartline/",
-  },
-]
 
 const Footer = () => {
   return (
     <footer className="text-gray-600 my-4 text-center">
       <div>© {new Date().getFullYear()} Eric Hartline</div>
-      <div className="mt-2">
-        {links.map((item) => (
-          <Link key={item.name} href={item.url}>
-            <a className="mr-2">{item.name}</a>
-          </Link>
-        ))}
-      </div>
+      <SocialIcons />
       <NowPlaying />
     </footer>
   )
