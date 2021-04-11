294bbce877c932d708598d9a21354a63f4651812
wildlifehexagon <hartline.eric@gmail.com>
2021-04-11 16:00:08 -0500

    refactor: replace indigo color

--- components/ProjectIcon.tsx
+++ components/ProjectIcon.tsx
@@ -2,7 +2,7 @@ import { FaReact } from "react-icons/fa"
 
 const ProjectIcon = ({ icon }: { icon: string }) => {
   return (
-    <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+    <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-green-700 flex-shrink-0">
       {icon === "react" && <FaReact size="5em" color="#61DAFB" />}
     </div>
   )
