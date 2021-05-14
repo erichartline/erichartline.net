223a603d2a5614c479178bb9df6f485bdc55f4cf
wildlifehexagon <hartline.eric@gmail.com>
2021-05-13 21:59:11 -0500

    feat: add go and node icons

--- components/ProjectIcon.tsx
+++ components/ProjectIcon.tsx
@@ -1,4 +1,6 @@
 import { FaReact } from "react-icons/fa"
+import { FaNodeJs } from "react-icons/fa"
+import GoIcon from "./GoIcon"
 
 type Props = {
   icon: string
@@ -10,6 +12,8 @@ const ProjectIcon = ({ icon, color }: Props) => {
     <div
       className={`sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-${color} flex-shrink-0`}>
       {icon === "react" && <FaReact size="5em" color="#61DAFB" />}
+      {icon === "go" && <GoIcon color="#7fd5ea" />}
+      {icon === "nodejs" && <FaNodeJs size="5em" />}
     </div>
   )
 }
