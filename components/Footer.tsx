8f218954ae96eee1ce794fe5eb37b73c10f12cb4
Eric Hartline <hartline.eric@gmail.com>
2022-03-01 18:05:11 -0600

    feat: add spotify now playing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
 import SocialIcons from "./SocialIcons"
-// import NowPlaying from "./NowPlaying"
+import NowPlaying from "./NowPlaying"
 
 const Footer = () => {
   return (
     <footer className="text-gray-600 my-4 text-center text-lg">
       <div>© {new Date().getFullYear()} Eric Hartline</div>
       <SocialIcons />
-      {/* <NowPlaying /> */}
+      <NowPlaying />
     </footer>
   )
 }
--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -14,11 +14,11 @@ const NowPlaying = () => {
       <span className="inline-flex">
         {nowPlaying ? (
           <React.Fragment>
-            <span className="mr-2">
+            <span className="mr-2 text-base">
               <em>Now Playing...</em>
             </span>
             <a
-              className="text-gray-800 font-medium"
+              className="text-gray-800 text-base"
               href={data.songUrl}
               target="_blank"
               rel="noopener noreferrer">
@@ -26,10 +26,10 @@ const NowPlaying = () => {
             </a>
           </React.Fragment>
         ) : (
-          <span className="text-gray-800 font-medium">Not Playing</span>
+          <span className="text-gray-800 text-base">Not Playing</span>
         )}
         <span className="mx-2 text-gray-500 hidden sm:block">{" – "}</span>
-        <span className="text-gray-500 dark:text-gray-300">
+        <span className="text-gray-500 dark:text-gray-300 text-base">
           {data?.artist ?? "Spotify"}
         </span>
       </span>
