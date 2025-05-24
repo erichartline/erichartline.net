80631529cab9fe1e35a5d35097e64d5d7e6c1bf1
Eric Hartline <crossthebreeze@gmail.com>
2025-05-24 12:22:09 -0500

    fix: fix import

--- app/api/spotify-top-tracks/route.ts
+++ app/api/spotify-top-tracks/route.ts
@@ -1,5 +1,5 @@
-import { getTopTracks } from "@lib/spotify"
 import { NextResponse } from "next/server"
+import { getTopTracks } from "@/lib/spotify"
 
 export async function GET() {
   const response = await getTopTracks()
